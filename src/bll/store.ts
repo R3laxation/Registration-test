da94d0b67e9f769be9dc1b1a551dc50e2cca76f5
R3laxation <memories@bk.ru>
2022-09-13 19:38:20 +0600

    Added store

---
+++ src/bll/store.ts
@@ -0,0 +1,20 @@
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { appSlice } from "./slices/appSlice";
+import {registrationSlice} from "./slices/registrationSlice";
+
+
+const rootReducer = combineReducers({
+    app: appSlice,
+    registration: registrationSlice,
+});
+
+export const store = configureStore({
+        reducer: rootReducer,
+    }
+);
+
+
+export type AppRootStateType = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
