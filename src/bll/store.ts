db9be0e91187f49110e9fc09ff91492508cb52c1
R3laxation <memories@bk.ru>
2022-09-13 19:47:08 +0600

    Added react-redux lib

--- src/bll/store.ts
+++ src/bll/store.ts
@@ -1,4 +1,5 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
 
 import { appSlice } from "./slices/appSlice";
 import {registrationSlice} from "./slices/registrationSlice";
