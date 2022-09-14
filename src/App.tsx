9ecf10fe0447144758786f14ddeadee17d521c11
R3laxation <memories@bk.ru>
2022-09-14 19:41:25 +0600

    Added provider with store

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-import { PATH } from './common/constants';
-import { Login } from './pages/auth/login/Login';
-import {Home } from './components/home/Home';
-import { Registration } from './pages/auth/registration/Registration';
+import {PATH} from './common/constants';
+import {Home} from './components/home/Home';
 import {Profile} from "./pages/profile/Profile";
 import {News} from "./pages/news/News";
 import {Main} from "./pages/main/Main";
 import {AuthPage} from "./pages/auth/AuthPage";
-
 
 function App() {
   return (
@@ -16,6 +13,7 @@ function App() {
         <Home/>
         <Routes>
             <Route path={'/'} element={<Main/>}/>
+            <Route path={PATH.MAIN} element={<Main/>}/>
             <Route path={PATH.AUTH} element={<AuthPage/>}/>
             <Route path={PATH.NEWS} element={<News/>}/>
             <Route path={PATH.PROFILE + '/*'} element={<Profile/>}/>
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 export const Main = () => {
     return (
         <div>
-            Main
+            <div>
+                Main
+            </div>
+
         </div>
     );
 };
